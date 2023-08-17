08ece7b179bfbed26ad09bb4a1ffba3a1f193fb8
Harini Sri <harinisri@Harinis-MacBook-Air.local>
2023-08-17 19:12:26 +0530

    second commit

---
+++ frontend/src/screens/BillwiseSummary.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const BillwiseSummary = () => {
+  return (
+    <div>BillwiseSummary</div>
+  )
+}
+
+export default BillwiseSummary
---
+++ frontend/src/screens/ModifyBill.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ModifyBill = () => {
+  return (
+    <div>ModifyBill</div>
+  )
+}
+
+export default ModifyBill
